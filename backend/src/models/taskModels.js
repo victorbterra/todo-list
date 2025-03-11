0b32214eeb010cb095b51909dab0f54095afb545
Victor Terra <victorbterra@gmail.com>
2025-03-11 11:46:55 -0300

    feat: -Atualização do Schema incluindo o campo (user) para criar relação das tasks com a conta e o campo status para definir o status da tarefa.

--- backend/src/models/taskModels.js
+++ backend/src/models/taskModels.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 const taskSchema = new mongoose.Schema({
   title: { type: String, required: true },  // Campo obrigatório
   description: { type: String, default: "" }, // Opcional
+  status: { type: String, enum: ["pendente", "concluída"], default: "pendente" },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // Referência ao usuário
   completed: { type: Boolean, default: false }, 
   priority: { type: String, enum: ["Baixa", "Média", "Alta"], default: "Média" }, 
   dueDate: { type: Date, default: null },  
